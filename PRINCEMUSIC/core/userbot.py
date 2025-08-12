0eae33bf7444f0fcc055c937a0f10df816413a0d
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-08-12 10:25:32 +0530

    Update userbot.py

--- PRINCEMUSIC/core/userbot.py
+++ PRINCEMUSIC/core/userbot.py
@@ -20,35 +20,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="SHUKLAAss1",
+            name="PRINCEAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="SHUKLAAss2",
+            name="PRINCEAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="SHUKLAAss3",
+            name="PRINCEAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="SHUKLAAss4",
+            name="PRINCEAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="SHUKLAAss5",
+            name="PRINCEAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
